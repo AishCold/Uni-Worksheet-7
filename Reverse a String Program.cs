d507cdbd57685320b5a6ed3e8967be9a4a73c52b
AishCold <72552823+AishCold@users.noreply.github.com>
2021-01-16 23:12:16 +0000

    Add files via upload

---
+++ Reverse a String Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L7_1_ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse a string application");
+            Console.WriteLine("Input a word or phrase:");
+
+            //user types in input, string variable is created to contain the user's input
+
+            string userInput = Console.ReadLine();
+
+            //first step to reverse the user's input: convert the string into a character array
+
+            char[] inputArray = userInput.ToCharArray();
+
+            //a loop that loops over half of the array
+            //As we iterate over each element in the loop we will swap it with the corresponding element in the other half of the array.
+
+            for(int i = 0; i < inputArray.Length / 2; i++)
+            {
+                char temp = inputArray[i];
+                inputArray[i] = inputArray[inputArray.Length - 1 - i];
+                inputArray[inputArray.Length - 1 - i] = temp;
+            }
+
+            userInput = new string(inputArray);
+
+            Console.WriteLine(userInput);
+        }
+    }
+}
